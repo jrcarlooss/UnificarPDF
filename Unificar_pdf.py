bf9e18ca4af241589b71e2f45c6a4bf48bbe14f2
CARLOS ALVES <68497704+jrcarlooss@users.noreply.github.com>
2024-01-17 09:18:43 -0300

    Add files via upload

---
+++ Unificar_pdf.py
@@ -0,0 +1,30 @@
+import os
+import fitz  # PyMuPDF
+
+# Diretório contendo os arquivos PDF a serem combinados
+diretorio = r'C:\Users\PC-CM-BONSUCESSO\Downloads\pdf'
+
+# Lista todos os arquivos no diretório
+arquivos = [arquivo for arquivo in os.listdir(diretorio) if arquivo.lower().endswith('.pdf')]
+
+# Objeto para armazenar o PDF combinado
+pdf_combinado = fitz.open()
+
+# Loop pelos arquivos no diretório e adicionar cada PDF ao objeto pdf_combinado
+for arquivo in arquivos:
+    caminho_arquivo = os.path.join(diretorio, arquivo)
+
+    # Abre cada arquivo PDF
+    pdf_documento = fitz.open(caminho_arquivo)
+
+    # Adiciona todas as páginas ao PDF combinado
+    pdf_combinado.insert_pdf(pdf_documento)
+
+# Caminho para o arquivo PDF combinado
+caminho_pdf_combinado = os.path.join(diretorio, 'arquivo_combinado.pdf')
+
+# Salva o PDF combinado em um novo arquivo
+pdf_combinado.save(caminho_pdf_combinado)
+pdf_combinado.close()
+
+print(f'Arquivos PDF combinados com sucesso. O arquivo combinado está em: {caminho_pdf_combinado}')
